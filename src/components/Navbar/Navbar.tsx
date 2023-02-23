7818591803bdb41ab81fb20f0adcef3b43edcc26
MateusLopes <mateus20.lopes02@gmail.com>
2023-02-22 22:10:11 -0300

    :sparkles: feat: add componente navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import logo from '../../assets/logoMateusLopes.svg'
+import './navbar.scss'
+
+const Navbar = () => {
+
+  return (
+    <nav className='navbar'>
+      <div className="navbar-container">
+        <img className='logo' src={logo} alt="Logomarca Mateus Lopes" />
+        <ul className='navbar-container-links'>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li><a href="#">Experiência</a></li>
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Habilidades</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+} 
+
+export { Navbar }
