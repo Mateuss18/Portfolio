2aab73ca481d81eaf24e872329bef57a596ba458
MateusLopes <mateus20.lopes02@gmail.com>
2023-02-24 23:44:18 -0300

    :sparkles: feat(header): add links nos buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import imagemMateus from '../../assets/fotoMateusLopes.svg'
 import iconLinkedin from '../../assets/iconLinkedin.svg'
 import iconGithub from '../../assets/iconGithub.svg'
 import iconDownload from '../../assets/iconDownload.svg'
+import curriculo from '../../assets/CV-Mateus_Lopes.pdf'
 import './header.scss'
 
 const Header = () => {
@@ -14,18 +15,18 @@ const Header = () => {
           <h1 className='right-name'>Mateus Antonio Rodrigues Lopes</h1>
           <p className='right-career'>Desenvolvedor Front End</p>
           <div className="social-links">
-            <button className='social-links-linkedin btn'>
+            <a className='social-links-linkedin btn' target={'_blank'} href='https://www.linkedin.com/in/mateus--lopes/'>
               <img src={iconLinkedin} alt="Icone do LinkedIn" />
               LinkedIn
-            </button>
-            <button className='social-links-github btn'>
+            </a>
+            <a className='social-links-github btn' target={'_blank'} href='https://github.com/Mateuss18'>
               <img src={iconGithub} alt="Icone do Github" />
               Github
-            </button>
-            <button className='social-links-download btn'>
+            </a>
+            <a className='social-links-download btn' href={curriculo} download target={'_blank'}>
               Baixar currículo
               <img src={iconDownload} alt="Icone de download" />
-            </button>
+            </a>
           </div>
         </div>
       </div>
