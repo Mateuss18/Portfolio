be72f2fca4137848ab77748537f8874237a331f8
MateusLopes <mateus20.lopes02@gmail.com>
2023-02-26 22:49:23 -0300

    :sparkles: feat(projects): add componente projects

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { Navbar } from "./components/Navbar/Navbar"
 import { Header } from "./components/Header/Header"
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
+import Projects from './components/Projects/Projects'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
       <Header />
       <About />
       <Experience />
-    </div>
+      <Projects />
+    </>
   )
 }
 
