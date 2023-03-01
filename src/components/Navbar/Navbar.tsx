8a8b0bfd95866c692f5b6b2cb7cded2649f56e0e
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-01 18:03:06 -0300

    :recycle: refactor: melhorando nomenclatura de classes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,12 +5,14 @@ const Navbar = () => {
 
   return (
     <nav className='navbar'>
-      <div className="navbar-container">
-        <img className='logo' src={logo} alt="Logomarca Mateus Lopes" />
-        <ul className='navbar-container-links'>
+      <div className="wrapper">
+        <a href="#">
+          <img className='logo' src={logo} alt="Logomarca Mateus Lopes" />
+        </a>
+        <ul className='menu'>
           <li><a href="#">Inicio</a></li>
           <li><a href="#">Sobre</a></li>
-          <li><a href="#">Experiência</a></li>
+          <li><a href="#">Experiências</a></li>
           <li><a href="#">Projetos</a></li>
           <li><a href="#">Habilidades</a></li>
           <li><a href="#">Contato</a></li>
