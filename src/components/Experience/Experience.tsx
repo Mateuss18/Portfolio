1ef996b11a20144bb5e10a676c4500d71020f578
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-01 19:45:45 -0300

    :sparkles: feat(experience): add segundo card

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -4,8 +4,24 @@ function Experience() {
   return (
     <section id="experience">
       <div className="wrapper">
-        <h2 className="title">Expêriencia</h2>
+        <h2 className="title">Expêriencias</h2>
         <div className="container">
+          <div className="card">
+            <div className="head">
+              <h3 className="card-title">
+                Desenvolvedor Front-End Freelancer
+              </h3>
+              <span className="card-time">Fevereiro 2022 - atualmente ({new Date().getMonth()} mêses)</span>
+            </div>
+            <div className="info">
+              <div className="company">
+                Freelancer
+              </div>
+              <p className="description">
+                Faço o desenvolvimento de páginas web utilizando HTML5, SASS, JavaScript e React. Também realizo o desenvolvimento do design das páginas através do software Figma.
+              </p>
+            </div>
+          </div>
           <div className="card">
             <div className="head">
               <h3 className="card-title">
