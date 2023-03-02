a352637f5f6617a96710ab836d51e8d63db795df
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-01 23:15:45 -0300

    :sparkles: feat(project): add componente project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './styles/app.scss'
-import { Navbar } from "./components/Navbar/Navbar"
-import { Header } from "./components/Header/Header"
+import Navbar from "./components/Navbar/Navbar"
+import Header from "./components/Header/Header"
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
 import Projects from './components/Projects/Projects'
---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,44 @@
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import ProjectCard from './ProjectCard/ProjectCard'
+import testeImage from '../../assets/teste.png'
+import './projects.scss'
+
+const Projects = () => {
+  const responsive = {
+      0: { items: 1 },
+      568: { items: 2 },
+      1250: { items: 3, itemsFit: 'contain' },
+  };
+
+  const items = [
+    <ProjectCard name='Projeto 1' imageLink='https://raw.githubusercontent.com/Mateuss18/Electrum/main/assets/readme-image.png' githubLink='https://github.com/Mateuss18/Electrum' deployLink='https://mateuss18.github.io/Electrum/' />,
+    <ProjectCard name='Projeto 2' imageLink='https://raw.githubusercontent.com/Mateuss18/Agency/main/src/assets/readme-image.png' githubLink='#' deployLink='#' />,
+    <ProjectCard name='Projeto 3' imageLink='https://raw.githubusercontent.com/Mateuss18/Electrum/main/assets/readme-image.png' githubLink='#' deployLink='#' />,
+    <ProjectCard name='Projeto 4' imageLink='https://raw.githubusercontent.com/Mateuss18/Electrum/main/assets/readme-image.png' githubLink='#' deployLink='#' />,
+    <ProjectCard name='Projeto 5' imageLink='https://raw.githubusercontent.com/Mateuss18/Electrum/main/assets/readme-image.png' githubLink='#' deployLink='#' />,
+    <ProjectCard name='Projeto 6' imageLink='https://raw.githubusercontent.com/Mateuss18/Electrum/main/assets/readme-image.png' githubLink='#' deployLink='#' />,
+  ];
+
+  return (
+    <section id='projects'>
+      <div className="wrapper">
+        <h2 className='title'>Projetos</h2>
+        <AliceCarousel
+          mouseTracking
+          autoPlay
+          autoPlayInterval={2000}
+          animationDuration={950}
+          autoPlayStrategy={'default'}
+          disableButtonsControls={true}
+          items={items}
+          responsive={responsive}
+          controlsStrategy="responsive"
+          keyboardNavigation={true}
+        />
+      </div>
+    </section>
+  )
+}
+
+export default Projects
