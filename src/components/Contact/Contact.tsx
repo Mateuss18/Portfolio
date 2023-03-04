3279c260595b0ebb1dd14e4883ebb99982e25e6d
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-03 23:47:23 -0300

    :sparkles: feat(contact): add componente contact

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,21 @@
+
+
+function Contact() {
+  return (
+    <section id="contact">
+      <div className="wrapper">
+        <button className="email">mateus20.lopes02@gmail.com</button>
+        <div className="social-contact">
+          <button className="whatsapp btn">
+            <img src="" alt="Icone Whatsapp" />
+          </button>
+          <button className="linkedin btn">
+            <img src="" alt="Icone LinkedIn" />
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
