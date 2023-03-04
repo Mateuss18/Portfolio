b4a6ff7238998f1f43803fb974ea1ab4db63f6d1
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-04 18:17:50 -0300

    :sparkles: feat(contact): add links no componente

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,17 +1,20 @@
-
+import linkedinIcon from './assets/linkedin-btn.svg';
+import whatsappIcon from './assets/whatsapp-btn.svg';
+import './contact.scss';
 
 function Contact() {
   return (
     <section id="contact">
       <div className="wrapper">
+        <h2 className='title'>Contato</h2>
         <button className="email">mateus20.lopes02@gmail.com</button>
         <div className="social-contact">
-          <button className="whatsapp btn">
-            <img src="" alt="Icone Whatsapp" />
-          </button>
-          <button className="linkedin btn">
-            <img src="" alt="Icone LinkedIn" />
-          </button>
+          <a href='https://wa.me/5519996489694' target={'_blank'} className="whatsapp btn">
+            <img src={whatsappIcon} alt="Icone Whatsapp" />
+          </a>
+          <a href='https://www.linkedin.com/in/mateus--lopes/' target={'_blank'} className="linkedin btn">
+            <img src={linkedinIcon} alt="Icone LinkedIn" />
+          </a>
         </div>
       </div>
     </section>
