12ca3a651d932f3eb05d390f0877bdc8a129507b
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-06 22:52:35 -0300

    :sparkles: feat: add import do footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Experience from './components/Experience/Experience'
 import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </>
   )
 }
