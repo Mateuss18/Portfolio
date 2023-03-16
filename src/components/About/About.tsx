abae5ed39517428bea6c316c6885f7576d7db803
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-16 17:01:30 -0300

    :sparkles: feat(about): atualizando informacoes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -10,13 +10,13 @@ function About() {
           <img className='ilustration' src={ilustracao} alt="Ilustração de um desenvolvedor" />
           <div className='description'>
             <p>
-              Com 2 anos de experiência em desenvolvimento de projetos web e graduado em Análise e Desenvolvimento de Sistemas possuo amplo conhecimento nas tecnologias Front End para criar soluções de qualidade, dinâmicas e responsivas.
+              Com 2 anos de experiência em desenvolvimento de projetos web e graduado em Análise e Desenvolvimento de Sistemas, possuo amplo conhecimento nas tecnologias Front End para criar soluções de qualidade, dinâmicas e responsivas.
             </p>
             <p>
-              Também sou apaixonado por fazer atividade fisica algo que me ajuda muito a manter o foco nas tarefas do dia a dia.
+              Também sou apaixonado por fazer atividade física, algo que me ajuda muito a manter o foco e a disciplina nas tarefas do dia a dia.
             </p>
             <p>
-              Atualmente busco oportunidades para adquirir experiência profissional e aprimorar minhas habilidades em um ambiente de trabalho dinâmico e desafiador.
+              Atualmente trabalho como desenvolvedor freelancer, contudo tenho como objetivo trabalhar em como desenvolvedor fixo em um ambiente de trabalho dinâmico e desafiador.
             </p>
           </div>
         </div>
