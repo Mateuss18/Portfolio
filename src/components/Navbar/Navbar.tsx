689197d125332021a6be4f7026d6266ed0027d75
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-16 11:27:11 -0300

    :sparkles: feat: add menu mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,35 @@
 import logo from './assets/logoMateusLopes.svg'
+import menuOpen from './assets/menu-hamburger.svg'
+import menuClose from './assets/menu-close.svg'
 import './navbar.scss'
+import { useState } from 'react'
 
 function Navbar () {
+  const [active, setMode] = useState(false)
+  const ToggleMode = () => {
+    setMode(!active)
+  }
 
   return (
-    <nav className='navbar'>
-      <div className="wrapper">
-        <a href="#">
-          <img className='logo' src={logo} alt="Logomarca Mateus Lopes" />
+    <nav className={active ? 'navbar activeMenu' : 'navbar'}>
+      <div className='wrapper'>
+        <a href="#" className='logo'>
+          <img src={logo} alt="Logomarca Mateus Lopes" />
         </a>
         <ul className='menu'>
-          <li><a href="#header">Inicio</a></li>
-          <li><a href="#about">Sobre</a></li>
-          <li><a href="#experience">Experiências</a></li>
-          <li><a href="#projects">Projetos</a></li>
-          <li><a href="#skills">Habilidades</a></li>
-          <li><a href="#contact">Contato</a></li>
+          <li><a href="#header" onClick={ToggleMode}>Inicio</a></li>
+          <li><a href="#about" onClick={ToggleMode}>Sobre</a></li>
+          <li><a href="#experience" onClick={ToggleMode}>Experiências</a></li>
+          <li><a href="#projects" onClick={ToggleMode}>Projetos</a></li>
+          <li><a href="#skills" onClick={ToggleMode}>Habilidades</a></li>
+          <li><a href="#contact" onClick={ToggleMode}>Contato</a></li>
         </ul>
+        <button className={active ? 'btn-open' : 'btn-open active'} onClick={ToggleMode}>
+          <img src={menuOpen} alt="Abrir menu" />
+        </button>
+        <button className={active ? 'btn-close active' : 'btn-close'} onClick={ToggleMode}>
+          <img src={menuClose} alt="Fechar menu" />
+        </button>
       </div>
     </nav>
   )
