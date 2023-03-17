587d58ee24e4bb4042710052c7d75322afabd111
MateusLopes <mateus20.lopes02@gmail.com>
2023-03-17 16:22:49 -0300

    :mag: melhorando SEO

--- src/components/Projects/ProjectCard/ProjectCard.tsx
+++ src/components/Projects/ProjectCard/ProjectCard.tsx
@@ -6,14 +6,14 @@ const ProjectCard = ({name, imageLink, githubLink, deployLink}: any) => {
   return (
     <div className='card-project'>
       <h3 className='card-project-title'>{name}</h3>
-      <img className="card-project-image" src={imageLink} alt="" />
+      <img className="card-project-image" src={imageLink} alt={name} />
       <div className="card-project-links">
         <a href={githubLink} target='_blank' className="link">
-          <img src={iconGithub} alt="" />
+          <img src={iconGithub} alt="Github link" />
           <span>Repositório</span>
         </a>
         <a href={deployLink} target='_blank' className="link">
-          <img src={iconDeploy} alt="" />
+          <img src={iconDeploy} alt="Link de deploy" />
           <span>Ver projeto</span>
         </a>
       </div>
