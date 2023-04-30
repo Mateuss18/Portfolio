e926ba4f9526939edac364a341de84dc92d60b81
MateusLopes <mateus20.lopes02@gmail.com>
2023-04-30 16:36:09 -0300

    :sparkles: feat(experience): atualizando experiencias

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,4 +1,5 @@
-import './experience.scss'
+import ExperienceCard from "./ExperienceCard";
+import "./experience.scss";
 
 function Experience() {
   return (
@@ -6,43 +7,35 @@ function Experience() {
       <div className="wrapper">
         <h2 className="title">Expêriencias</h2>
         <div className="container">
-          <div className="card">
-            <div className="head">
-              <h3 className="card-title">
-                Desenvolvedor Front-End Freelancer
-              </h3>
-              <span className="card-time">Fevereiro 2022 - atualmente ({new Date().getMonth()} meses)</span>
-            </div>
-            <div className="info">
-              <div className="company">
-                Freelancer
-              </div>
-              <p className="description">
-                Faço o desenvolvimento de páginas web utilizando HTML5, SASS, JavaScript e React. Também realizo o desenvolvimento do design das páginas através do software Figma.
-              </p>
-            </div>
-          </div>
-          <div className="card">
-            <div className="head">
-              <h3 className="card-title">
-                Estágiario em Análise de dados
-              </h3>
-              <span className="card-time">Outubro 2022 - Dezembro 2022 (2 meses)</span>
-            </div>
-            <div className="info">
-              <div className="company">
-                Bitflowin
-              </div>
-              <p className="description">
-                Estágio em análise de dados na empresa Bitflowin. 
-                Fui responsável pela criação de tabelas SQL para analise de estatísticas de empresas para melhorar estratégias de marketing e divulgação utilizando a tecnologia Google Big Query. 
-              </p>
-            </div>
-          </div>
+          <ExperienceCard
+            title="Desenvolvedor Front End"
+            time={`Abril 2023 - Atualmente ${new Date().getMonth()} meses`}
+            company="Agência Delucca"
+            description="Desenvolvimento e atualizações de páginas para e-commerces e criação de post utilizando Wordpress"
+          />
+
+          <ExperienceCard
+            title="Desenvolvedor Front-End Freelancer"
+            time="Fevereiro 2023 - Abril 2023"
+            company="Freelancer"
+            description="Faço o desenvolvimento de páginas web utilizando HTML5, SASS,
+            JavaScript e React. Também realizo o desenvolvimento do design
+            das páginas através do software Figma."
+          />
+
+          <ExperienceCard
+            title="Estágiario em Análise de dados"
+            time="Outubro 2022 - Abril 2022"
+            company="Bitflowin"
+            description="Estágio em análise de dados na empresa Bitflowin. Fui
+            responsável pela criação de tabelas SQL para analise de
+            estatísticas de empresas para melhorar estratégias de marketing
+            e divulgação utilizando a tecnologia Google Big Query."
+          />
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
